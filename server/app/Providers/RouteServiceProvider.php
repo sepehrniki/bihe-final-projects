1909a90fb0ad2d8135dbc0206f7007c46eac2c17
sepehr niki <niki.sphr@gmail.com>
2023-03-04 19:11:43 +0330

    * Removing additional parts of the framework for the api-server * Configure project env

--- server/app/Providers/RouteServiceProvider.php
+++ server/app/Providers/RouteServiceProvider.php
@@ -10,25 +10,15 @@ use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * The path to the "home" route for your application.
-     *
-     * Typically, users are redirected here after authentication.
-     *
-     * @var string
-     */
-    public const HOME = '/home';
+    public const HOME = '/';
 
-    /**
-     * Define your route model bindings, pattern filters, and other route configuration.
-     */
     public function boot(): void
     {
         $this->configureRateLimiting();
 
         $this->routes(function () {
             Route::middleware('api')
-                ->prefix('api')
+//                ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
@@ -36,9 +26,6 @@ class RouteServiceProvider extends ServiceProvider
         });
     }
 
-    /**
-     * Configure the rate limiters for the application.
-     */
     protected function configureRateLimiting(): void
     {
         RateLimiter::for('api', function (Request $request) {
